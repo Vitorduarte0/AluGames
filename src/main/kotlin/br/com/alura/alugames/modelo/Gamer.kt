34788a16935c69d8538e3df5e8645241fbcf8cfa
Vitorduarte0 <vitorsanta.cal.10@gmail.com>
2024-04-15 22:50:06 -0300

    Adicionando a criacao de um gamer, adicionando validacoes e manipulando listas

--- src/main/kotlin/br/com/alura/alugames/modelo/Gamer.kt
+++ src/main/kotlin/br/com/alura/alugames/modelo/Gamer.kt
@@ -1,11 +1,15 @@
 package br.com.alura.alugames.modelo
 
+import java.util.Scanner
 import kotlin.random.Random
 
 data class Gamer(
     var name: String, var email: String
 ){
     var dataNascimento: String? = null
+        set(value) {
+            field = value
+        }
     var usuario: String? = null
         set(value) {
             field = value
@@ -17,6 +21,7 @@ data class Gamer(
     var idInterno: String? = null
         get
 
+    val jogosBuscado = mutableListOf<Jogo?>()
     constructor(name: String, email: String, dataNascimento: String, usuario: String, idInterno: String? = null) :
             this(name, email) {
                 this.dataNascimento = dataNascimento
@@ -36,4 +41,44 @@ data class Gamer(
 
     }
 
+    fun validaEmail(): String {
+        val regex = Regex(
+            "^(?=.{1,64}@)[A-Za-z0-9_-]+(\\.[A-Za-z0-9_-]+)*@"
+                    + "[^-][A-Za-z0-9-]+(\\.[A-Za-z0-9-]+)*(\\.[A-Za-z]{2,})$"
+        )
+
+        if (!regex.matches(email)) {
+            return throw IllegalArgumentException("Email invalido")
+        }else {
+            return email
+        }
+    }
+
+    init {
+        if (name.isNullOrBlank()) {
+            throw IllegalArgumentException("Nome invalidooo!")
+        }
+        validaEmail()
+    }
+
+    companion object {
+        fun criarGamer(leitura: Scanner): Gamer {
+            println("Aeeeee, boas vindas ao AluGames! Vamos fazer seu cadastro \n")
+            println("Insira o nome:")
+            val nome = leitura.nextLine()
+            println("Insira o email:")
+            val email = leitura.nextLine()
+            println("Deseja completar seu cadastro? S/N")
+            val opcao = leitura.nextLine()
+            if (opcao.equals("s", true)) {
+                println("Insira a data de nascimento (DD/MM/AAAA): ")
+                val dataNascimento = leitura.nextLine()
+                println("Insira o nome do seu usuario: ")
+                val usuario = leitura.nextLine()
+                return Gamer(nome, email, dataNascimento, usuario)
+            }else {
+                return Gamer(nome, email)
+            }
+        }
+    }
 }
--- src/main/kotlin/br/com/alura/alugames/principal/Main.kt
+++ src/main/kotlin/br/com/alura/alugames/principal/Main.kt
@@ -1,35 +1,71 @@
 import br.com.alura.alugames.modelo.Gamer
 import br.com.alura.alugames.modelo.Jogo
 import br.com.alura.alugames.servicos.ConsumoApi
+import java.lang.IllegalArgumentException
 import java.util.Scanner
 
 fun main() {
-   val scanner = Scanner(System.`in`)
-   println("Digite um código de jogo para buscar:")
-   val busca = scanner.nextLine()
+    val scanner = Scanner(System.`in`)
+    val gamer = Gamer.criarGamer(scanner)
+    do {
+        println("Digite um código de jogo para buscar:")
+        val leitura = scanner.nextLine()
+        val buscaJogo = ConsumoApi().buscaJogoPorId(leitura)
+        var meuJogo: Jogo? = null
 
-   val buscaJogo = ConsumoApi().buscaJogoPorId(busca)
+        val resultado = runCatching {
+            meuJogo = Jogo(buscaJogo.info.title, buscaJogo.info.thumb)
+        }
 
-   var meuJogo: Jogo? = null
+        resultado.onFailure {
+            println("Cara, falhou aqui nessa bomba!!")
+        }
 
-   val resultado = runCatching {
-      meuJogo = Jogo(buscaJogo.info.title, buscaJogo.info.thumb)
+        resultado.onSuccess {
+            println("Deseja incluir uma descrição personalida? S/N")
+            val opcao = scanner.nextLine()
+            if (opcao.equals("s", true)) {
+                println("Insira a descrição do filme: ${meuJogo?.titulo}")
+                val descricaoDoUsuario = scanner.nextLine()
+                meuJogo?.descricao = descricaoDoUsuario
+            } else {
+                meuJogo?.descricao = meuJogo?.titulo
+            }
+
+            gamer.jogosBuscado.add(meuJogo)
+        }
+        println(meuJogo)
+
+        println("Deseja buscar mais um jogo? S/N")
+        val continuaBusca = scanner.nextLine()
+
+    } while (continuaBusca.equals("s", true))
+
+    println("Lista de jogos sem esta ordenada ${gamer.jogosBuscado}")
+
+    println("\n Lista ordenada por titulo: ")
+    gamer.jogosBuscado.sortBy {
+        it?.titulo
+    }
+    gamer.jogosBuscado.forEach {
+        println(it)
+    }
+    println("\n lista de jogos filtrado")
+    gamer.jogosBuscado.filter { jogo ->
+        jogo?.titulo?.contains("the Darkness") ?: false
+    }
+
+   println("Deseja remover um item da lista?")
+   val opcao = scanner.nextLine()
+   if (opcao.equals("s", true)) {
+      println("Digite a posicao do item a ser excluido")
+      val posicao = scanner.nextInt()
+      if (posicao > gamer.jogosBuscado.size) {
+         throw IllegalArgumentException("Operacao invalida, pois o numero digita e maior que a quantidade de itens na lista")
+      }
+      gamer.jogosBuscado.removeAt(posicao)
+      println("\n Minha lista apos excluir um item")
+      println(gamer.jogosBuscado)
+
    }
-   resultado.onFailure {
-      println("Cara, falhou aqui nessa bomba!!")
-   }
-
-  resultado.onSuccess {
-     println("Deseja incluir uma descrição personalida? S/N")
-     val opcao = scanner.nextLine()
-     if (opcao.equals("s", true)) {
-        println("Insira a descrição do filme: ${meuJogo?.titulo}")
-        val descricaoDoUsuario = scanner.nextLine()
-        meuJogo?.descricao = descricaoDoUsuario
-     } else {
-        meuJogo?.descricao = meuJogo?.titulo
-     }
-  }
-   println(meuJogo)
-
 }
